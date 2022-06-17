5932f648841a8c047ea09b7d7e5a7029894d1686
ElenaLatynina <latynina.elena.v@gmail.com>
2022-06-17 11:15:41 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+function getExtremeElements(array) {
+  // Change code below this line
+  array = getExtremeElements(array).splice(1, array.length - 2);
+
+  // Change code above this line
+}
+console.log([1, 2, 3, 4, 5]);
+console.log(["Earth", "Mars", "Venus"]);
+console.log(["apple", "peach", "pear", "banana"]);
+
+console.log;
+
